854b44ccf12fde9d7f985e07851f9bc0eb510f80
Puvis Teeweang <59161111@go.buu.ac.th>
2019-08-20 19:37:46 +0700

    image and reset

--- app/src/main/java/buu/informatics/cs59161111/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/cs59161111/diceroller/MainActivity.kt
@@ -3,15 +3,19 @@ package buu.informatics.cs59161111.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
+
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
@@ -19,21 +23,25 @@ class MainActivity : AppCompatActivity() {
         val resetButton: Button = findViewById(R.id.reset_button)
         resetButton.setOnClickListener { reset() }
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice Rolled!"
+
     }
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
 
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
 
     }
     private fun reset() {
-
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "0"
-
+        diceImage.setImageResource(R.drawable.empty_dice)
     }
 }
